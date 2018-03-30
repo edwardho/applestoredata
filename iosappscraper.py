598fddf14848199c6bead36c6c3ff52b15aa4a98
Edward Ho <ekinbenho@gmail.com>
2018-03-30 15:49:07 -0400

    Add files via upload
    Updated iosappscraper.py to return package names, handle exceptions, and propertly read utf-8 characters

--- iosappscraper.py
+++ iosappscraper.py
@@ -7,22 +7,40 @@ fp = open('iosscraperoutput.csv', 'wb')
 reader = csv.reader(f)
 writer = csv.writer(fp, delimiter=',')
 
-writer.writerows([['App ID', 'Publisher Name', 'App Name', 'Description', 'Primary Category']])
+writer.writerows([['iTunes ID', 'Bundle Id', 'App Name', 'Publisher Name', 'Primary Category']])
+
+
 
 for row in reader:
-    	response = urllib2.urlopen('http://itunes.apple.com/lookup?id=' + ', '.join(map(str, row))) # it's a file like object and works just like a file
-    	appjson = response.read()
-    	appdata = json.loads(appjson)
-    	aid = appdata['results'][0]['artistId']
-    	#print aid
-    	pubname = (appdata['results'][0]['artistName'])
-    	#print pubname
-    	appname = (appdata['results'][0]['trackCensoredName'])
-    	#print appname
-    	desc = (appdata['results'][0]['description'])
-    	#print desc
-    	pcat = (appdata['results'][0]['primaryGenreName'])
-    	#print pcat
-    	#print '\n'
-    	writer.writerows([[aid, pubname, appname, desc.encode('utf-8'),pcat]])
 
+        try:
+                response = urllib2.urlopen('http://itunes.apple.com/lookup?id=' + ', '.join(map(str, row))) # it's a file like object and works just like a file
+                appjson = response.read()
+                appdata = json.loads(appjson)
+
+                if (appdata['resultCount'] != 0) :
+                        aid = row[0]
+                        #print apple id
+                        bid = (appdata['results'][0]['bundleId'])
+                        #print bid
+                        pubname = (appdata['results'][0]['sellerName'])
+                        #print pubname
+                        appname = (appdata['results'][0]['trackName'])
+                        #print appname
+                        pgenre = (appdata['results'][0]['primaryGenreName'])
+                        #print pgenre
+                else:
+                        aid = row[0]
+                        bid = 'NULL'
+                        pubname = 'NULL'
+                        appname = 'NULL'
+                        pgenre = 'NULL'
+
+        except urllib2.HTTPError as e:
+                aid = row[0]
+                bid = 'NULL'
+                pubname = 'NULL'
+                appname = 'NULL'
+                pgenre = 'NULL'
+
+        writer.writerows([[aid, bid, appname.encode('utf-8'), pubname.encode('utf-8'), pgenre]])
